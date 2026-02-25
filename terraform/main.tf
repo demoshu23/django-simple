9d5d5e9f2f5f4e004dc414456f3bc1fd10bd8d2b
unknown <demoshu2022@gmail.com>
2026-02-25 01:06:19 -0500

    version issue

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,8 +34,8 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.1.0"        # Module version
 
-  name            = var.cluster_name
-  cluster_version = "1.29"  # Kubernetes version
+  cluster_name    = var.cluster_name   # ✅ Correct argument
+  cluster_version = "1.29"             # Kubernetes cluster version
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
