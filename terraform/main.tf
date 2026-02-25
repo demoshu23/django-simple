aa01f22de3f92fc72025f6f5756f4bdecc536291
unknown <demoshu2022@gmail.com>
2026-02-25 11:38:50 -0500

    work flow dispatch

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,11 +11,11 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.18.1"
 
-  name             = "django-vpc"
-  cidr             = "10.0.0.0/16"
-  azs              = slice(data.aws_availability_zones.available.names, 0, 2)
-  public_subnets   = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
+  name            = "django-vpc"
+  cidr            = "10.0.0.0/16"
+  azs             = slice(data.aws_availability_zones.available.names, 0, 2)
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
 
   enable_nat_gateway = true
   single_nat_gateway = true
@@ -34,7 +34,7 @@ module "eks" {
   version = "20.1.0"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.29"   # Kubernetes cluster version
+  cluster_version = "1.29" # Kubernetes cluster version
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
