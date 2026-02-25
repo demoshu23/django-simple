7db61958b24404457254ca92c2da2cba935c25a0
unknown <demoshu2022@gmail.com>
2026-02-25 00:44:55 -0500

    update main.tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,38 +1,19 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  name    = "django-vpc"
-  cidr    = "10.0.0.0/16"
-
-  azs             = ["us-east-1a", "us-east-1b"]
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
-}
-
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "django-eks"
-  cluster_version = "1.29"
+  version         = "20.0.1"
+
+  name    = var.cluster_name      # <- was cluster_name
+  version = "1.29"                # <- was cluster_version
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_groups = {
     default = {
-      min_size     = 2
-      max_size     = 5
-      desired_size = 2
-      instance_types = ["t3.medium"]
+      desired_capacity = 2
+      max_capacity     = 5
+      min_capacity     = 2
+      instance_types   = ["t3.medium"]
     }
   }
-}
-
-resource "aws_ecr_repository" "django" {
-  name = "django-enterprise"
 }
