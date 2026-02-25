8c51d38b2e874fcbc8a8e7693245d630f4a953dd
unknown <demoshu2022@gmail.com>
2026-02-25 01:09:53 -0500

    version

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,16 +5,17 @@ provider "aws" {
 # ---------------------
 # VPC Module
 # ---------------------
+data "aws_availability_zones" "available" {}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.18.1"
 
-  name = "django-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = slice(data.aws_availability_zones.available.names, 0, 2)
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+  name             = "django-vpc"
+  cidr             = "10.0.0.0/16"
+  azs              = slice(data.aws_availability_zones.available.names, 0, 2)
+  public_subnets   = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
 
   enable_nat_gateway = true
   single_nat_gateway = true
@@ -25,17 +26,15 @@ module "vpc" {
   }
 }
 
-data "aws_availability_zones" "available" {}
-
 # ---------------------
 # EKS Cluster
 # ---------------------
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.1.0"        # Module version
+  version = "20.1.0"
 
-  cluster_name    = var.cluster_name   # ✅ Correct argument
-  cluster_version = "1.29"             # Kubernetes cluster version
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"   # Kubernetes cluster version
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
