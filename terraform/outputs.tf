3ef82e61079784b463f4ed7a0a299695b454c710
unknown <demoshu2022@gmail.com>
2026-02-25 00:16:36 -0500

    terraform

---
+++ terraform/outputs.tf
@@ -0,0 +1,34 @@
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+  description = "Private subnet IDs"
+  value       = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  description = "Public subnet IDs"
+  value       = module.vpc.public_subnets
+}
+
+output "eks_cluster_name" {
+  description = "EKS cluster name"
+  value       = module.eks.cluster_id
+}
+
+output "eks_cluster_endpoint" {
+  description = "EKS API server endpoint"
+  value       = module.eks.cluster_endpoint
+}
+
+output "eks_cluster_security_group_id" {
+  description = "EKS cluster security group ID"
+  value       = module.eks.cluster_security_group_id
+}
+
+output "ecr_repository_url" {
+  description = "ECR repository URI"
+  value       = aws_ecr_repository.django.repository_url
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "cluster_name" {
+  description = "EKS cluster name"
+  type        = string
+  default     = "django-eks"
+}
+
+variable "ecr_repository" {
+  description = "Name of ECR repository for Django images"
+  type        = string
+  default     = "django-enterprise"
+}
